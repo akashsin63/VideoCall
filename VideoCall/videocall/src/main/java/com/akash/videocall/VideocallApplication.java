823bff03e70af4f61b9e08dd37a6786e19e12bfa
Akash <akashsingh54812@gmail.com>
2024-02-18 15:45:34 +0530

    added users

--- VideoCall/videocall/src/main/java/com/akash/videocall/VideocallApplication.java
+++ VideoCall/videocall/src/main/java/com/akash/videocall/VideocallApplication.java
@@ -1,13 +1,43 @@
 package com.akash.videocall;
 
+import com.akash.videocall.user.User;
+import com.akash.videocall.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideocallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideocallApplication.class, args);
+
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	) {
+		return args -> {
+			service.register(User.builder()
+					.username("akash")
+					.email("akash@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("aniket")
+					.email("aniket@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("alok")
+					.email("alok@mail.com")
+					.password("aaa")
+					.build());
+		};
+	}
 }
+
